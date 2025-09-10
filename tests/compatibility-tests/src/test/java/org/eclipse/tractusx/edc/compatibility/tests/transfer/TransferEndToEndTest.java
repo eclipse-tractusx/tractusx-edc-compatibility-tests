1f550ef5b62729e10d78d267db3347df24c3cea9
Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>
2025-09-10 09:19:18 +0200

    Reflect BdrsClient changes for mocking
    Signed-off-by: Lars Geyer-Blaumeiser <lars.blaumeiser@cofinity-x.com>

--- tests/compatibility-tests/src/test/java/org/eclipse/tractusx/edc/compatibility/tests/transfer/TransferEndToEndTest.java
+++ tests/compatibility-tests/src/test/java/org/eclipse/tractusx/edc/compatibility/tests/transfer/TransferEndToEndTest.java
@@ -67,7 +67,6 @@ import static org.eclipse.tractusx.edc.compatibility.tests.fixtures.DcpHelperFun
 import static org.eclipse.tractusx.edc.compatibility.tests.fixtures.DcpHelperFunctions.configureParticipantContext;
 import static org.mockserver.integration.ClientAndServer.startClientAndServer;
 
-
 @EndToEndTest
 public class TransferEndToEndTest {
 
@@ -118,9 +117,21 @@ public class TransferEndToEndTest {
             Runtimes.CONTROL_PLANE.create("local-control-plane")
                     .configurationProvider(() -> POSTGRESQL.configFor(LOCAL_PARTICIPANT.getName()))
                     .configurationProvider(LOCAL_PARTICIPANT::controlPlaneConfig)
-                    .registerServiceMock(BdrsClient.class, DIDS::get)
-                    .registerServiceMock(AudienceResolver.class, message -> Result.success(DIDS.get(message.getCounterPartyId())))
-    );
+                    .registerServiceMock(BdrsClient.class, new BdrsClient() {
+                        @Override
+                        public String resolveDid(String bpn) {
+                            return DIDS.get(bpn);
+                        }
+
+                        @Override
+                        public String resolveBpn(String did) {
+                            return DIDS.entrySet().stream()
+                                    .filter(entry -> entry.getValue().equals(did))
+                                    .findFirst().orElseThrow().getKey();
+                        }
+                    })
+                    .registerServiceMock(AudienceResolver.class, message -> Result
+                            .success(DIDS.get(message.getCounterPartyId()))));
 
     @Order(2)
     @RegisterExtension
